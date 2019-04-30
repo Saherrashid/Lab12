a06d67a6e075c01a97084f12eb4a20d5d5091453
saherrashid <srashi20@illinois.edu>
2019-04-30 12:31:04 -0500

    FINAL FOR REAL

--- app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/spring2019/lab12/MainActivity.java
@@ -35,10 +35,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
             public void onClick(View view) {
-                findViewById(R.id.searcher).setOnClickListener(v -> {
-                    Vibrator z = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                    z.vibrate(500);
-                });
                 String input = editText.getText().toString();
                 Request request = new Request(this, textView, input);
                 url = dictionaryEntries(input);
